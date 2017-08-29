ac9842def754c2f26f1641ad2dd674a77586e918
Jess Gardner <inanna1130@gmail.com>
2017-08-29 15:22:02 -0400

    full crud movies model

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -28,12 +28,14 @@ class MoviesController < ApplicationController
     end
 
     def destroy
-
+        @movie = Movie.find(params[:id])
+        @movie.destroy
+        redirect_to movies_path
     end
 
     private
     def movie_params
-        params.require(:movie).permit(:title, :genre, :synopsis)
+        params.require(:movie).permit(:title, :year, :genre, :synopsis, :picture)
     end
 
 end
